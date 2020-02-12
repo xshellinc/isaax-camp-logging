51bc0cd6fb9c1c68948851ff4594433b3c598d89
tomotomo <tomotomobile@gmail.com>
2020-02-12 09:59:31 +0900

    remove unused code

--- isaax-camp-logging.php
+++ isaax-camp-logging.php
@@ -15,36 +15,6 @@ require __DIR__.'/vendor/autoload.php';
  */
 
 /**
- * ログイン時のリダイレクト先をトップページへ
- */
-add_action( 'admin_init', function () {
-	if( !current_user_can('edit_posts') ){
-		$home_url = site_url('', 'http');
-		wp_safe_redirect($home_url);
-		exit();
-	}
-} );
-
-/**
- * ログアウト時のリダイレクト先をトップページへ
- */
-add_action( 'wp_logout', function () {
-	$home_url = site_url('', 'http');
-	wp_safe_redirect($home_url);
-	exit();
-});
-
-/**
- * 購読者がログイン時に管理バーを表示させない
- */
-add_filter( 'show_admin_bar', function ($content) {
-	if( current_user_can("edit_posts") ){
-		return $content;
-	}
-	return false;
-});
-
-/**
  * GAにUser-IDを送信する
  */
 add_action( 'wp_footer', function () {
